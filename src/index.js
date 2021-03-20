38fb8499d886404fdb6bf9eab7ae398c29c5d699
unknown <davyberra@gmail.com>
2021-03-19 20:08:17 -0700

    started working with components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,86 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';
 
+//function tick() {
+//    const element = (
+//        <div>
+//            <h1>Hello, Davy!</h1>
+//            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//        </div>
+//    );
+//    ReactDOM.render(element, document.getElementById('root'));
+//}
 
-const name = 'Davy Berra';
-const element = <h1>Hello, {name}</h1>;
+//setInterval(tick, 1000);
 
-reactDom.render(
-    element,
+//function Welcome(props) {
+//    return <h1>Welcome, {props.name}.</h1>;
+//}
+
+//function Avatar(props) {
+//    return (
+//        )
+//}
+
+//    <comment userinfo-name="Davy" />,
+//    document.getElementById('root')
+//);
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <div className="UserInfo-name">
+                <strong>{props.name} says...</strong>
+            </div>
+        </div>
+    );
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo name={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+        </div>
+    );
+}
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, {this.props.name}!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Clock name="Davy" />,
     document.getElementById('root')
 );
